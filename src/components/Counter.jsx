392537959f334e1a179298ce74b59997c332cdbb
Adam Baker <adam_baker@live.co.uk>
2022-04-13 23:02:42 +0100

    Added a reset button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -15,15 +15,21 @@ const Counter = () => {
         </h2>
         <button
           onClick={() => setCount(count - 1)}
-          class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mx-4 my-10 border border-blue-500 hover:border-transparent rounded"
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mx-4 my-10 border border-blue-500 hover:border-transparent rounded"
         >
           -
         </button>
         <button
           onClick={() => setCount(count + 1)}
-          class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mx-4 my-10 border border-blue-500 hover:border-transparent rounded"
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mx-4 my-10 border border-blue-500 hover:border-transparent rounded"
         >
           +
+        </button>
+        <button
+          onClick={() => setCount(0)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Reset
         </button>
       </div>
     </>
